68533500d76845bb066d6f9435ed912b653a517c
Manuel <manuelh0107g@gmail.com>
2021-10-06 16:54:40 -0500

    charge page

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+const port = 7000;
+
+app.use(express.static('public'));
+
+app.listen(port, ()=>{ 
+	console.log('Server running on port '+ port);
+}); 
